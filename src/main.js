4dc6afd8750f10129d54320086d57d18b4a18a0d
Anna Tomchenko <anytom6996@gmail.com>
2026-05-24 16:27:17 +0300

    added image search app

---
+++ src/main.js
@@ -0,0 +1,68 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+import { fetchImages } from './src/js/pixabay-api.js';
+
+import {
+  createGallery,
+  clearGallery,
+} from './src/js/render-functions.js';
+
+const form = document.querySelector('.form');
+const loader = document.querySelector('.loader');
+
+hideLoader();
+
+form.addEventListener('submit', async event => {
+  event.preventDefault();
+
+  const searchText = event.target.elements['search-text'].value.trim();
+
+  if (!searchText) {
+    iziToast.error({
+      message: 'Please fill in the search field!',
+      position: 'topRight',
+    });
+
+    return;
+  }
+
+  clearGallery();
+
+  showLoader();
+
+  try {
+    const data = await fetchImages(searchText);
+
+    if (data.hits.length === 0) {
+      iziToast.error({
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+        position: 'topRight',
+      });
+
+      return;
+    }
+
+    createGallery(data.hits);
+  } catch (error) {
+    iziToast.error({
+      message: 'Something went wrong!',
+      position: 'topRight',
+    });
+
+    console.log(error);
+  } finally {
+    hideLoader();
+  }
+
+  form.reset();
+});
+
+function showLoader() {
+  loader.classList.add('is-visible');
+}
+
+function hideLoader() {
+  loader.classList.remove('is-visible');
+}
